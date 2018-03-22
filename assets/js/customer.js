011b4464ec2d8abb036a679767eec9039767a3fc
Joshua Bowers <joshua-bowers@outlook.com>
2018-03-22 15:07:09 -0400

    Update customer.js

--- assets/js/customer.js
+++ assets/js/customer.js
@@ -1,6 +1,6 @@
 var $form = $('form#test-form'),
     url = 'https://script.google.com/macros/s/AKfycbxTJ4rpFX6BuTHHVlIfcw8eNEsthOIqoUDbdpiKiQA1XdO7DHUI/exec'
-// submit below
+// submit form below
 $('#submit-form').on('click', function(e) {
   e.preventDefault();
   var jqxhr = $.ajax({
