adc8909964d43a5aff31c6b78c10177a5f75c0f6
Rayshawn Eatmon <r2eatmon@gmail.com>
2018-03-23 10:40:34 -0400

    cleaned up code

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,39 @@
+
+if(confirm("Simulate Accident? Ok=Yes Cancel=No")){
+    $(document).ready(function () {
+    $("#status1").text('Accident Detected!');
+      $("#status2").text('Please See Diagnostics Panel and submit claim.');
+      $("#frontOfCar").addClass("damaged");
+      $("#rightSide").addClass("damaged");
+      $("#damageListFront").append('<li>Front Panel</li><br/><li>Headlights</li><br/>');
+      $("#damageListRight").append('<li>Side Mirror</li><br/><li>Window Panel</li><br/>');
+      $("#frontIcon").remove();
+      $("#rightIcon").remove();
+      $("#engineLightIndicator").addClass("damagedLight");
+      $("#airbagIndicator").addClass("damagedLight");
+});
+}else{
+//alert("NO Accident!!!");
+}
+
+
+
+function DisplayCurrentTime() {
+
+var date = new Date();
+var hours = date.getHours() > 12 ? date.getHours() - 12 : date.getHours();
+var am_pm = date.getHours() >= 12 ? "PM" : "AM";
+hours = hours < 10 ? "0" + hours : hours;
+var minutes = date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+var seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
+time = hours + ":" + minutes + ":" + seconds + " " + am_pm;
+var lblTime = document.getElementById("time");
+lblTime.innerHTML = time;
+var timeout = setTimeout(DisplayCurrentTime, 1000);
+};
+
+
+
 var $form = $('form#test-form');
 var policyNumber = 0;
 var url = 'https://script.google.com/macros/s/AKfycbxTJ4rpFX6BuTHHVlIfcw8eNEsthOIqoUDbdpiKiQA1XdO7DHUI/exec';
